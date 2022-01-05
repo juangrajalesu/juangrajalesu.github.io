ee40193c9a85c433b74c5fb8d400a880b9fe54cf
juangrajalesu <juan.grajalesu@udea.edu.co>
2022-01-05 15:15:15 -0500

    Añadiendo toast para el envio del form

--- src/components/ContactMe/index.js
+++ src/components/ContactMe/index.js
@@ -1,28 +1,41 @@
 import { EmailForm } from "../EmailForm";
 import { Container, Mail } from "./styles";
 import { Emoji } from "../Emoji";
-import { useState} from "react";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const ContactMe = () => {
   const [isEmailSent, setIsEmailSent] = useState(false);
+  const nofitfy = () => toast.dark("El contacto fue enviado exitosamente 🥳");
   const handleContent = () => {
     setIsEmailSent(!isEmailSent);
+    nofitfy();
   };
-  
+
   return (
     <Container id='ContactMe'>
-      <h1>Contáctame</h1>
-      <p>
-        Llena el siguiente formulario o envia un e-mail a
-        <Mail href='mailto: juan.grajalesu@gmail.com'>
-          {" "}
-          mi correo <Emoji emoji='📧' />
-        </Mail>
-      </p>
       {isEmailSent ? (
-        <p>El contacto fue enviado exitosamente </p>
+        <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar={true}
+          closeOnClick
+          pauseOnHover
+          toastClassName={'dark-toast'}
+        />
       ) : (
-        <EmailForm handleClick={handleContent}/>
+        <>
+          <h1>Contáctame</h1>
+          <p>
+            Llena el siguiente formulario o envia un e-mail a
+            <Mail href='mailto: juan.grajalesu@gmail.com'>
+              {" "}
+              mi correo <Emoji emoji='📧' />
+            </Mail>
+          </p>
+          <EmailForm handleClick={handleContent} />
+        </>
       )}
     </Container>
   );
